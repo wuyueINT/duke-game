b522bc0b2cd9b0767624aca2e8d397627e456590
wuyue <kaoooooooooo@163.com>
2020-07-23 10:03:12 +0800

    [v1.0]增加Kotlin组件
    Signed-off-by: wuyue <kaoooooooooo@163.com>

--- app/src/main/java/com/wuyue/countinggame/StatelistRecyclerAdapter.java
+++ app/src/main/java/com/wuyue/countinggame/StatelistRecyclerAdapter.java
@@ -19,7 +19,6 @@ public class StatelistRecyclerAdapter extends RecyclerView.Adapter<StatelistRecy
     List<String> stateList;
     QuestionlistRecyclerAdapter questionsAdapter;
 
-
     public StatelistRecyclerAdapter(Context context, List<String> questionList, List<String> stateList,
                                     QuestionlistRecyclerAdapter questions) {
         this.context = context;
@@ -66,4 +65,6 @@ public class StatelistRecyclerAdapter extends RecyclerView.Adapter<StatelistRecy
             tv_item2 = itemView.findViewById(R.id.tv_item2);
         }
     }
+
+
 }
